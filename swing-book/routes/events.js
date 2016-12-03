38a37c81e8efb7c2ed2d7ca8984488abb1e5245f
michaeltangelo <tanman24689@gmail.com>
2016-12-03 17:56:44 -0500

    updated route handler for events page and events view

--- swing-book/routes/events.js
+++ swing-book/routes/events.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
-var Events = mongoose.model('Event');
+var Event = mongoose.model('Event');
 var passport = require('passport');
 
 // As with any middleware it is quintessential to call next()
@@ -14,15 +14,40 @@ var isAuthenticated = function (req, res, next) {
 }
 
 router.get('/', function(req, res, next) {
-	Events.find({}).sort('-date').exec(function(err, events) {
-		console.log("Events: " + events);
-		// res.render('events', {user: req.user});
-		res.send('Parsed events.');
+	Event.find({}).sort('-date').exec(function(err, events) {
+		res.render('events', {events: events});
 	});
 });
 
 router.get('/create', isAuthenticated, function(req, res, next) {
-	res.send('here');
+	console.log(Date.now());
+	res.render('events-create');
 });
+
+router.post('/create', isAuthenticated, function(req, res, next) {
+	console.log(req.body);
+	var newEvent = new Event();
+	newEvent.date = new Date(req.body.date);
+	newEvent.start = req.body.start;
+	newEvent.end = req.body.end;
+	newEvent.name = req.body.name;
+	var recurring = req.body.recurring;
+	newEvent.location = req.body.location;
+	if (req.body.imageUrl) {
+		newEvent.imageUrl = req.body.imageUrl;
+	}
+
+	if (req.body.description) {
+		newEvent.description = req.body.description;
+	}
+	newEvent.save(function(err) {
+		if (err) {
+			console.log("Error saving event: " + err);
+			throw err;
+		}
+		console.log("Successfully saved event.");
+		res.redirect('/events');
+	});
+})
 
 module.exports = router;
